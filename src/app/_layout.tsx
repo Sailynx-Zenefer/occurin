3e684bcd5c399928c220d303cb3d9b76b6c47a20
sailynx-zenefer <sailynx-zenefer@localhost.localdomain>
2024-08-05 18:20:14 +0100

    theme fix

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,8 +3,7 @@ import {
   PaperProvider,
   MD3LightTheme,
   MD3DarkTheme,
-  adaptNavigationTheme,
-  MD3Colors,
+  adaptNavigationTheme,MD3Colors
 } from "react-native-paper";
 import {
   DarkTheme as NavigationDarkTheme,
@@ -14,32 +13,31 @@ import {
 import { AuthProvider } from "../hooks/Auth";
 import { AlertsProvider } from "react-native-paper-alerts";
 import { useColorScheme } from "react-native";
-
 import merge from "deepmerge";
 
-import { Colors } from "../constants/colors";
+// import { Colors } from "../constants/colors";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
-const customDarkTheme = { ...MD3DarkTheme, colors: MD3Colors };
-const customLightTheme = { ...MD3LightTheme, colors: MD3Colors };
+// const customDarkTheme = { ...MD3DarkTheme, colors: MD3Colors.dark };
+// const customLightTheme = { ...MD3LightTheme, colors: Colors.light };
 
 const { LightTheme, DarkTheme } = adaptNavigationTheme({
   reactNavigationLight: NavigationDefaultTheme,
   reactNavigationDark: NavigationDarkTheme,
 });
 
-const CombinedLightTheme = merge(LightTheme, customLightTheme);
-const CombinedDarkTheme = merge(DarkTheme, customDarkTheme);
+const CombinedLightTheme = merge(LightTheme, MD3LightTheme);
+const CombinedDarkTheme = merge(DarkTheme, MD3DarkTheme);
 
 const queryClient = new QueryClient();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const paperTheme = CombinedLightTheme
-    // colorScheme === "dark" ? CombinedDarkTheme : CombinedLightTheme;
+  // const colorScheme = useColorScheme();
+  // const paperTheme =
+  //   colorScheme === "dark" ? CombinedDarkTheme : CombinedLightTheme;
 
   return (
-    <PaperProvider theme={paperTheme}>
+    <PaperProvider theme={CombinedLightTheme}>
       <ThemeProvider value={CombinedLightTheme}>
         <AuthProvider>
           <QueryClientProvider client={queryClient}>
